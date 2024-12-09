a3f0ac4f49129028b72ea7fef91471589cbe0448
Zike145 <kakalgk4@gmail.com>
2024-12-09 20:08:11 -0300

    Primeiro Commit

---
+++ Projeto/aula.js
@@ -0,0 +1,7 @@
+var texto = "Este é um texto"
+var numero = 3
+var booleano = true
+
+console.log ("Aqui é uma String: " + texto);
+console.log ("Aqui é uma variável do tipo number: " + numero);
+console.log ("Aqui é uma varíavel de tipo boolean: " + booleano);
