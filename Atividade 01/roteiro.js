7bb714afb07169a0c5dc66032c633e09359f3d1d
Zike145 <kakalgk4@gmail.com>
2024-12-12 19:52:23 -0300

    Enviando atividades

--- Atividade 01/roteiro.js
+++
@@ -1,5 +0,0 @@
-let Nome = "Juan Kainã"
-let Idade = 18
-let NotaFinal = 9.5
-
-console.log ("Nome: ", nome, "Idade: ", idade, "Nota Final: ", NotaFinal)
---
+++ Atividade01/roteiro.js
@@ -0,0 +1,5 @@
+let nome = "Fábio Oliveira"
+let idade = 18
+let notaFinal = 9.5
+
+console.log ("Nome: ", nome, "Idade: ", idade, "Nota Final: ", notaFinal)
